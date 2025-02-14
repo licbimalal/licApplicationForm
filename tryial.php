74a56a87e3190527a8c28471ea0196b4db84543f
licbimalal <licbimalal@gmail.com>
2025-02-14 14:17:48 +0530

    io

--- tryial.php
+++ tryial.php
@@ -1,5 +1,5 @@
 <?php
-$to="";
-$subject=;
-$from=;
+$to="C:\Users\USER\AppData\Local\GitHubDesktop\app-3.4.8\licApplicationForm\index.html";
+$subject="insurance";
+$from="bhashkar.branjan@gmail.com";
 ?>
